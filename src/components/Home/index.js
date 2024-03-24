68e788b4c06cd6f3548cd18706c6f9d134dac8d9
FENG <982685807@qq.com>
2024-03-24 18:43:42 +0800

    home page responsible code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -31,9 +31,10 @@ const Home = () => {
     return (
         <>
             <div className="information-area">
-                <p> <span>Hello!</span> I am <span>Ziqing Feng</span> (Haruko Feng)!
-                    <br/>I am a passionate <span>Software Engineer</span> with a <span>Master's degree in Computer Science</span>.
-                    <br/>I am <span>currently living in China</span>. 
+                <div className="dot"></div>
+                <p><span>Hello!</span> I am <span>Ziqing Feng</span> (Haruko Feng) !
+                    <br/>I am a <span>Software Engineer</span> with a <span>Master's degree in Computer Science</span>.
+                    <br/>I am currently <span>living in China</span>. 
                     <br/>Regarding my language proficiency, I can speak <span>Chinese</span>, <span>Japanese</span>, 
                      and <span>English</span>.</p>
                 <img src={illustration} alt="illution"></img>
