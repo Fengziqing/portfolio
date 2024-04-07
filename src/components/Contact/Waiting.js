e95a35b04350c5ffd0717244caa8122332a4e42a
FENG <982685807@qq.com>
2024-04-07 17:23:26 +0800

    clean input area when message sended successfully

--- src/components/Contact/Waiting.js
+++ src/components/Contact/Waiting.js
@@ -4,7 +4,7 @@ import sending_email from '../../assets/sending-email.svg';
 const Waiting = () => {
     return (
     <div className='wait-popup-window'>
-        <img src={sending_email}></img>
+        <img src={sending_email} alt='email sending'></img>
         <p>Your Message is Sending...<br/> Please wait for a moment... <br/> <span>*You will receive a confirmation email if the message is sent successfully.*</span></p>
     </div>
     );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,11 +7,9 @@ import Waiting from './Waiting';
 import Blur from './Blur';
 
 const Contact = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: ''
-    });
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [message,setMessage] = useState('');
     const [needBlur, setNeedBlur] = useState(false);
     const [waiting, setWaiting] = useState(false);
     const [sendOk, setSendOk] = useState(false);
@@ -25,45 +23,40 @@ const Contact = () => {
         setSendFailed(false);
         setSendOk(false);
     }
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
     };
-    const handleSend = async () => {
-        console.log(formData);
-        if (!formData.name) {
-            setName_error_visiable(true);
-            setTimeout(() => {
-                setName_error_visiable(false);
-            }, 5000);
-            return;
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+    const handleMessageChange = (e) => {
+        setMessage(e.target.value);
+    };
+
+    async function sendingData(){
+        const sendData = {
+            'name': name,
+            'email': email,
+            'message': message
         }
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(formData.email)) {
-            setEmail_error_visible(true);
-            setTimeout(() => {
-                setEmail_error_visible(false);
-            }, 5000);
-            return;
-        }
-        if (!formData.message) {
-            setMessage_error_visible(true);
-            setTimeout(() => {
-                setMessage_error_visible(false);
-            }, 5000);
-            return;
-        }
+        console.log(sendData);
         setSendButton('Sending');
         setNeedBlur(true);
         setWaiting(true);
-        const data = JSON.stringify(formData);
+        const data = JSON.stringify(sendData);
         axios.defaults.withCredentials = true;
+
         try {
             await axios.post('https://vercel-express-eosin.vercel.app/api/contact', data, {
-                // axios.post('http://localhost:3001/api/contact', data, {
+                // axios.post('http://localhost:3000/api/contact', data, {
                 headers: {
                     'Content-Type': 'application/json',
                 }
             }).then(response => {
+                setName('');
+                setEmail('');
+                setMessage('');
                 setSendOk(true);
             });
         } catch (error) {
@@ -73,6 +66,34 @@ const Contact = () => {
         setSendButton('Send');
         setWaiting(false)
         setNeedBlur(false);
+    }
+
+    function handleSend (e) {
+        e.preventDefault();
+        //data checking
+        if (name === '') {
+            setName_error_visiable(true);
+            setTimeout(() => {
+                setName_error_visiable(false);
+            }, 5000);
+            return;
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            setEmail_error_visible(true);
+            setTimeout(() => {
+                setEmail_error_visible(false);
+            }, 5000);
+            return;
+        }
+        if (message==='') {
+            setMessage_error_visible(true);
+            setTimeout(() => {
+                setMessage_error_visible(false);
+            }, 5000);
+            return;
+        }
+        sendingData();
     }
 
     return (
@@ -85,16 +106,18 @@ const Contact = () => {
                 </p>
                 <div className="contact-block">
                     {needBlur && <Blur />}
+                    <form onSubmit={handleSend}>
                     <p className='title'>Name *</p>
-                    <input type="text" name='name' minLength="1" maxLength="20" onChange={handleChange}></input>
+                    <input type="text" name='name' minLength="1" maxLength="20" value={name} onChange={handleNameChange}></input>
                     {name_error_visible && <p className="name-error">At least, let me know your name 🥺🙏</p>}
                     <p className='title'>Email Address *</p>
-                    <input type="email" name='email' maxLength="50" onChange={handleChange}></input>
+                    <input type="email" name='email' maxLength="50" value={email} onChange={handleEmailChange}></input>
                     {email_error_visible && <p className="email-error">Correct email is necessary 🙋</p>}
                     <p className='title'>Message *</p>
-                    <textarea type="text" name='message' minLength="1" maxLength="500" placeholder="Type here..." onChange={handleChange}></textarea>
+                    <textarea type="text" name='message' minLength="1" value={message} maxLength="500" placeholder="Type here..." onChange={handleMessageChange}></textarea>
                     {message_error_visible && <p className="message-error">Are you sure you don't wanna say anything🥺🥺🥺</p>}
-                    <button className="submit-button" onClick={()=>{handleSend()}}>{sendButton}</button>
+                    <button className="submit-button">{sendButton}</button>
+                    </form>
                 </div>
                 {waiting && <Waiting />}
                 {sendOk && <SendEmailSucceed closeWindow={colsePopUpWindow} />}
