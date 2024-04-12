ec30d2812c5e37108a424bf73ff47c5b6b107b97
FENG <982685807@qq.com>
2024-04-12 23:34:29 +0800

    correction of apiflash url

--- src/components/BentoComponents/ClickHorizontalRectangle.js
+++ src/components/BentoComponents/ClickHorizontalRectangle.js
@@ -2,7 +2,7 @@ import './ClickHorizontalRectangle.scss';
 
 export default function ClickHorizontalRectangle(props){
     const {imgsrc,imgalt,title} = props;
-    const screenshot = 'https://api.apiflash.com/v1/urltoimage?access_key=0fd0abc22ea2401e8d8ab35db4e55d27&wait_until=page_loaded&url=https://ziqingfeng.vercel.app/Demo/DemoPage/&crop=10,20,50,100' + imgsrc;
+    const screenshot = 'https://api.apiflash.com/v1/urltoimage?access_key=0fd0abc22ea2401e8d8ab35db4e55d27&wait_until=page_loaded&url=https://ziqingfeng.vercel.app/Demo/DemoPage/' + imgsrc;
     return (
         <div className="click-horizontal-rectangle">
             <img src={screenshot} alt={imgalt}/>
